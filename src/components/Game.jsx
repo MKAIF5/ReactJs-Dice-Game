78d3524a1584b24a173de132af7a99b74c598d44
mohammed kaif <mkaif555123@gmail.com>
2024-12-25 19:25:35 +0500

    images download and lets start functionality

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Swal from 'sweetalert2'
 
 const Game = () => {
+
+    const numberSelect = () => {
+        Swal.fire({
+            imageUrl: "https://placeholder.pics/svg/300x1500",
+            imageHeight: 500,
+            imageAlt: "A tall image",
+            showCloseButton: true
+        });
+    }
+
     return (
-       <>
-       
-       </>
+        <>
+            <div>
+
+                <div className='my-5 text-center font-bold text-5xl text-gray-700'>
+                    <h1>Let's Play </h1>
+                </div>
+                <div>
+                    <button
+                        onClick={numberSelect}
+                        className='bg-orange-500 p-3'>Select The Number</button>
+                </div>
+            </div>
+        </>
     )
 }
 
